0265d706944352063bfb2f1a76f18b1931c4eecd
Константин <zuker_gta@mail.ru>
2024-10-22 19:15:14 +1000

    22/10/2024-1

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 import timeit
 
 
+
+# Количество циклов измерений
+number_=1000
+
 # Сортировка пузырьком
 def bubble_sort():
     arr = [64, 34, 37, 80, 94, 125, 55, 34, 47, 81, 5, 19, 71, 31, 22, 97, 101, 158, 111, 7, 1, 9, 161, 14, 29, 88]
@@ -16,8 +20,52 @@ def bubble_sort():
             break
 
 
-execution_time = timeit.timeit(bubble_sort, number=200)
-print(f"Среднее время выполнения: {execution_time} секунд")
+execution_time = timeit.timeit(bubble_sort, number=number_)
+print(f"Сортировка пузырьком. Среднее время выполнения: {execution_time} секунд")
+
+
+# Сортировка вставкой
+def insertion_sort():
+    arr = [64, 34, 37, 80, 94, 125, 55, 34, 47, 81, 5, 19, 71, 31, 22, 97, 101, 158, 111, 7, 1, 9, 161, 14, 29, 88]
+    # Проходим по всему массиву, начиная со второго элемента (т.к. первый элемент уже "отсортирован")
+    for i in range(1, len(arr)):
+        key = arr[i]  # текущий элемент, который нужно вставить в правильное место
+        j = i - 1
+
+        # Сдвигаем элементы, которые больше текущего (key), вправо
+        while j >= 0 and arr[j] > key:
+            arr[j + 1] = arr[j]
+            j -= 1
+
+        # Вставляем текущий элемент на правильное место
+        arr[j + 1] = key
+
+    return arr
+
+
+execution_time = timeit.timeit(insertion_sort, number=number_)
+print(f"Сортировка вставкой. Среднее время выполнения: {execution_time} секунд")
+
+
+# Сортировка выбором
+def selection_sort():
+    arr = [64, 34, 37, 80, 94, 125, 55, 34, 47, 81, 5, 19, 71, 31, 22, 97, 101, 158, 111, 7, 1, 9, 161, 14, 29, 88]
+    # Проходим по всем элементам массива
+    for i in range(len(arr)):
+        # Предполагаем, что текущий элемент — минимальный
+        min_idx = i
+
+        # Ищем минимальный элемент в оставшейся части массива
+        for j in range(i + 1, len(arr)):
+            if arr[j] < arr[min_idx]:
+                min_idx = j
+
+        # Меняем найденный минимальный элемент с текущим элементом
+        arr[i], arr[min_idx] = arr[min_idx], arr[i]
+
+
+execution_time = timeit.timeit(selection_sort, number=number_)
+print(f"Сортировка выбором. Среднее время выполнения: {execution_time} секунд")
 
 
 # Стандартная сортировка
@@ -26,6 +74,6 @@ def standard_sort():
     arr.sort()
 
 
-execution_time = timeit.timeit(standard_sort, number=200)
-print(f"Среднее время выполнения: {execution_time} секунд")
+execution_time = timeit.timeit(standard_sort, number=number_)
+print(f"Стандартная сортировка. Среднее время выполнения: {execution_time} секунд")
 
